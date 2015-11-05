561fc0fe653f86ea19fd6ef54e3a57a0d375812a
Travis Montgomery <travisbm@gmail.com>
2015-11-04 18:18:04 -0600

    follow and unfollow methods working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -88,4 +88,14 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
+
+    #Before Filters
+
+    #Confirms a logged in User
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
